6ad7cf6bd239cbc28c4f687efe07da9d12c114a6
Joshua George <joshua.vincent.george@gmail.com>
2024-01-04 21:36:33 -0800

    removed component imports and dummy data

--- src/pages/HomePage/home.js
+++ src/pages/HomePage/home.js
@@ -1,36 +1,26 @@
 import "./home.scss";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import Tile from "../../components/Tile/Tile";
 
 export default function Home() {
-	const dummyData = [
-		{
-			image: "http://dummyimage.com/439x260.png/ff4444/ffffff",
-			title: "Forgiven",
-			description:
-				"Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
-		},
-		{
-			image: "http://dummyimage.com/482x260.png/dddddd/000000",
-			title: "Black Christmas",
-			description:
-				"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
-		},
-		{
-			image: "http://dummyimage.com/481x260.png/ff4444/ffffff",
-			title: "Combien Tu M'aimes?",
-			description:
-				"Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
-		},
-	];
-	return (
-		<>
-			{/* <Sidebar /> */}
-			{dummyData.map((data) => {
-				return (
-					<Tile title={data.title} image={data.image} desc={data.description} />
-				);
-			})}
-		</>
-	);
+	// const dummyData = [
+	// 	{
+	// 		image: "http://dummyimage.com/439x260.png/ff4444/ffffff",
+	// 		title: "Forgiven",
+	// 		description:
+	// 			"Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+	// 	},
+	// 	{
+	// 		image: "http://dummyimage.com/482x260.png/dddddd/000000",
+	// 		title: "Black Christmas",
+	// 		description:
+	// 			"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+	// 	},
+	// 	{
+	// 		image: "http://dummyimage.com/481x260.png/ff4444/ffffff",
+	// 		title: "Combien Tu M'aimes?",
+	// 		description:
+	// 			"Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+	// 	},
+	// ];
+
+	return <></>;
 }
