ca45d8da95e966b50b73fcef964f5b20da3ef016
Kate Bark <barkkate@gmail.com>
2024-01-05 11:27:12 -0500

    updating comp re change of plan in layout and execution of guide page

---
+++ src/components/GuideProsCons/guideProsCons.js
@@ -0,0 +1,34 @@
+import "./guide.scss";
+
+function GuideProsCons() {
+  return (
+    // <div className="guide-..."
+  );
+}
+
+export default GuideProsCons;
+
+// function GuideComponent(props) {
+//   return (
+//     <div className="guide">
+//       <img
+//         className="guide__image"
+//         src={props.image}
+//       >
+//       </img>
+//       <h1 className="guide__title">
+//         Title here
+//         {props.title}
+//       </h1>
+//       <p className="guide__description">
+//         Description here........
+//         {props.description}
+//       </p>
+//       <button className="guide__button">
+//         Button Text
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default GuideComponent;
