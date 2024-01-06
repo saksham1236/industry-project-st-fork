def26c693f51b3c7d87bc652bad000cac957c815
Kate Bark <barkkate@gmail.com>
2024-01-06 16:05:03 -0500

    removing code with bugs

--- src/components/GuideInstructionsComp/guideInstructionsComp.js
+++ src/components/GuideInstructionsComp/guideInstructionsComp.js
@@ -9,90 +9,43 @@ function GuideInstructionsComp() {
   const { id } = useParams();
 
   console.log("useParams id: ", id);
+  console.log(Data[0].doSteps[0])
 
 
   return (
     <div className="guide-inst">
-      
-        {Data.filter(data => data.id === id)
-          .map(data => {
-            return (
-              <>
-              
-        
-              <div className="guide-inst__do-list">
-              <h1 className="guide-list__title">DO:</h1>
+
+      {Data.filter(data => data.id === id)
+        .map(data => {
+          return (
+            <>
+              <div key={data.id} className="guide-inst__do-list">
+                <h1 className="guide-list__title">DO:</h1>
                 <GuideListComponent
-                  key={data.id}
+                  
                   doSteps={data.doSteps}
                 />
-                </div>
-                <div className="guide-inst__image-container">
-                  <img
-                    src={data.image}
-                    alt="product to be disposed of"
-                    className="guide-inst__image">
-                  </img>
-                </div>
-                <div className="guide-inst__dont-list">
-                  <h1 className="guide-list__title">DO NOT:</h1>
-                  {/* {Data.filter(data => data.id === id)
-          .map(data => {
-            return ( */}
-                  <GuideListComponent
-                    key={data.id}
-                    dontSteps={data.dontSteps}
-                  // image={data.image}
-                  // dontSteps={data.dontSteps}
-                  />
-                  {/* ); */}
-                  {/* })} */}
-                </div>
-                
-              </>
-            );
-          })}
-      
-
-
-      {/* <div className="guide-inst__dont-list">
-        <h1 className="guide-list__title">DO NOT:</h1>
-        {Data.filter(data => data.id === id)
-          .map(data => {
-            return (
-        <GuideListComponent
-        key={data.id}
-                doSteps={data.doSteps}
+              </div>
+              <div className="guide-inst__image-container">
+                <img
+                  src={data.image}
+                  alt="product to be disposed of"
+                  className="guide-inst__image">
+                </img>
+              </div>
+              {/* <div className="guide-inst__dont-list">
+                <h1 className="guide-list__title">DO NOT:</h1>
+                <GuideListComponent
+                  key={data.id}
+                  dontSteps={data.dontSteps}
                 />
-            )}
-      </div> */}
+              </div> */}
+
+            </>
+          );
+        })}
     </div>
   );
 }
 
 export default GuideInstructionsComp;
-
-// function GuideComponent(props) {
-//   return (
-//     <div className="guide">
-//       <img
-//         className="guide__image"
-//         src={props.image}
-//       >
-//       </img>
-//       <h1 className="guide__title">
-//         Title here
-//         {props.title}
-//       </h1>
-//       <p className="guide__description">
-//         Description here........
-//         {props.description}
-//       </p>
-//       <button className="guide__button">
-//         Button Text
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default GuideComponent;
--- src/components/GuideListComp/guideListComp.js
+++ src/components/GuideListComp/guideListComp.js
@@ -1,18 +1,17 @@
 import "./guideListComp.scss"
 
 function GuideListComponent({ id, doSteps, image, dontSteps }) {
-  console.log({doSteps});
-  // console.log(id);
-  // console.log("Current id: ", id)
+
 
   return (
     <div className="guide-list">
       <div className="guide-list__list-container">
-        {doSteps.map((step) => (
-          <p className="guide-list__step">{step}</p>
-        ))}
-        {/* <img src={image} className="guide-list__image" alt="product to be disposed of"></img> */}
-        {/* <p className="guide-list__step"> {dontSteps}</p> */}
+        {doSteps.map((step, index) =>  (
+          <p key={index} className="guide-list__step">{step}</p>
+          // console.log(step)
+          ))}
+        {/* <img src={image} className="guide-list__image" alt="product to be disposed of"></img>
+        <p className="guide-list__step"> {dontSteps}</p> */}
       </div>
     </div>
   );
