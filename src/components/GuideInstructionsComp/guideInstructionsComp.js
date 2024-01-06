c3f5c476a0828982a2ae99dfc779a0825bee61bd
Kate Bark <barkkate@gmail.com>
2024-01-06 16:37:31 -0500

    bugs fixed. colors and styling to be changed

--- src/components/GuideInstructionsComp/guideInstructionsComp.js
+++ src/components/GuideInstructionsComp/guideInstructionsComp.js
@@ -4,48 +4,37 @@ import Data from "../../data/guides.json";
 import { useParams } from "react-router-dom";
 
 function GuideInstructionsComp() {
-  console.log(Data[0].image);
 
   const { id } = useParams();
 
-  console.log("useParams id: ", id);
-  console.log(Data[0].doSteps[0])
-
-
   return (
     <div className="guide-inst">
-
-      {Data.filter(data => data.id === id)
-        .map(data => {
-          return (
-            <>
-              <div key={data.id} className="guide-inst__do-list">
-                <h1 className="guide-list__title">DO:</h1>
-                <GuideListComponent
-                  
-                  doSteps={data.doSteps}
-                />
-              </div>
-              <div className="guide-inst__image-container">
-                <img
-                  src={data.image}
-                  alt="product to be disposed of"
-                  className="guide-inst__image">
-                </img>
-              </div>
-              {/* <div className="guide-inst__dont-list">
-                <h1 className="guide-list__title">DO NOT:</h1>
-                <GuideListComponent
-                  key={data.id}
-                  dontSteps={data.dontSteps}
-                />
-              </div> */}
-
-            </>
-          );
-        })}
+      {Data.filter((data) => data.id === id).map((data) => {
+        return (
+          <div className="guide-inst">
+            <div className="guide-inst__do-list">
+              <h1 className="guide-list__title">DO:</h1>
+              <GuideListComponent
+                key={data.id}
+                steps={data.doSteps}
+              />
+            </div>
+            <div className="guide-inst__image-container">
+              <img src={data.image} className="guide-inst__image"></img>
+            </div>
+            <div className="guide-inst__do-list">
+              <h1 className="guide-list__title">DO NOT:</h1>
+              <GuideListComponent
+                key={data.id}
+                steps={data.dontSteps}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
+
 
 export default GuideInstructionsComp;
--- src/components/GuideListComp/guideListComp.js
+++ src/components/GuideListComp/guideListComp.js
@@ -1,20 +1,19 @@
 import "./guideListComp.scss"
 
-function GuideListComponent({ id, doSteps, image, dontSteps }) {
+function GuideListComponent({ id, steps, image }) {
+	console.log(steps);
 
-
-  return (
-    <div className="guide-list">
-      <div className="guide-list__list-container">
-        {doSteps.map((step, index) =>  (
-          <p key={index} className="guide-list__step">{step}</p>
-          // console.log(step)
-          ))}
-        {/* <img src={image} className="guide-list__image" alt="product to be disposed of"></img>
-        <p className="guide-list__step"> {dontSteps}</p> */}
-      </div>
-    </div>
-  );
+	return (
+		<div className="guide-list">
+			<div className="guide-list__list-container">
+				{steps.map((step, index) => (
+					<p key={index} className="guide-list__step">
+						{step}
+					</p>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default GuideListComponent;
