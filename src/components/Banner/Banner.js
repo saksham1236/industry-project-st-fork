5b366276927e926fdb07bbb0d6142645cb5081b4
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 18:56:41 -0800

    changed jsx structure of banner + added useScroll and useTransform

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,27 @@
+import { useRef } from "react";
 import "./banner.scss";
-function Banner() {
+import { useScroll, useTransform } from "framer-motion";
+
+function Banner({ backgroundURL, foregroundURL }) {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "end end"],
+	});
+
+	const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+	const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+
 	return (
-		<>
-			<div className="banner">banner</div>
-		</>
+		<div ref={ref} className="banner">
+			<div className="banner__text">Hello World!</div>
+			<div
+				className="banner__background"
+				style={{ backgroundImage: `url(${backgroundURL})` }}></div>
+			<div
+				className="banner__foreground"
+				style={{ backgroundImage: `url(${foregroundURL})` }}></div>
+		</div>
 	);
 }
 
