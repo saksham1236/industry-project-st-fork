ef20dfc384ace47e676b6e09dfcb3b72d1ace46b
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 19:12:31 -0800

    adjusting animation

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,18 +6,21 @@ function Banner({ backgroundURL, foregroundURL }) {
 	const ref = useRef(null);
 	const { scrollYProgress } = useScroll({
 		target: ref,
-		offset: ["start end", "end end"],
+		offset: ["start start", "end start"],
 	});
 
-	const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-	const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+	const textY = useTransform(scrollYProgress, [0, 1], ["0%", "180%"]);
 
 	return (
 		<div ref={ref} className="banner">
-			<div className="banner__text">Hello World!</div>
+			<motion.div className="banner__text" style={{ y: textY }}>
+				Hello World!
+			</motion.div>
 			<div
 				className="banner__background"
-				style={{ backgroundImage: `url(${backgroundURL})` }}></div>
+				style={{
+					backgroundImage: `url(${backgroundURL})`,
+				}}></div>
 			<div
 				className="banner__foreground"
 				style={{ backgroundImage: `url(${foregroundURL})` }}></div>
