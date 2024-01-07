5761abb4d1ac854121d978d4fcadef89ad5bd29e
Kate Bark <barkkate@gmail.com>
2024-01-07 15:30:05 -0500

    fixed dynamic color render, mobile resp done

--- src/components/GuideReasons/guideReasons.js
+++ src/components/GuideReasons/guideReasons.js
@@ -2,23 +2,22 @@ import "./guideReasons.scss";
 import Data from "../../data/guides.json";
 import { useParams } from "react-router-dom";
 
-function GuideReasons() {
-  const { id } = useParams();
-
+function GuideReasons({ data }) {
   return (
-    <div className="guide-reasons">
+    <div
+      className="guide-reasons"
+      style={{
+        backgroundColor: `${data.card_color}`,
+        color: `${data.text_color}`
+      }}>
       <h1 className="guide-reasons__title">Environmental Impact</h1>
       <div className="guide-reasons__container">
-        {Data.filter((data) => data.id === id).map((item) => (
-          <div key={item.id}>
-            {item.reasons.map((reason, index) => (
-              <p key={index} className="guide-reasons__list-item">{reason}</p>
-            ))}
-          </div>
+        {data.reasons.map((reason, index) => (
+          <p key={index} className="guide-reasons__list-item">{reason}</p>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default GuideReasons;
