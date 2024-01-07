904c8137ef3ec23dd631b7ddb4404acb7d052fda
Kate Bark <barkkate@gmail.com>
2024-01-07 00:53:17 -0500

    colours to be updated when mockup complete, resp added to instructions container

--- src/components/GuideReasons/guideReasons.js
+++ src/components/GuideReasons/guideReasons.js
@@ -3,19 +3,19 @@ import Data from "../../data/guides.json";
 import { useParams } from "react-router-dom";
 
 function GuideReasons() {
-
   const { id } = useParams();
-
-  const listOfReasons = Data.filter((data) => data.id === id) 
-  console.log(reasons)
 
   return (
     <div className="guide-reasons">
-      <h1 className="guide-list__title">Environmental Impact:</h1>
+      <h1 className="guide-reasons__title">Environmental Impact</h1>
       <div className="guide-reasons__container">
-        {listOfReasons.map(reason => {
-          <p className="guide-reasons__list-item">{reason}</p>
-        })}
+        {Data.filter((data) => data.id === id).map((item) => (
+          <div key={item.id}>
+            {item.reasons.map((reason, index) => (
+              <p key={index} className="guide-reasons__list-item">{reason}</p>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   )
