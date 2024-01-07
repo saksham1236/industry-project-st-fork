cdd976cab16c474dc7f327db1d98a78774e3871f
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 19:17:38 -0800

    added title prop for banner

--- src/pages/GuidePage/GuidePage.js
+++ src/pages/GuidePage/GuidePage.js
@@ -1,15 +1,18 @@
 import "./guidePage.scss";
 import GuideInstructionsComp from "../../components/GuideInstructionsComp/guideInstructionsComp";
 import Banner from "../../components/Banner/Banner";
-import background from "../../assets/images/background-Recyclable.png";
-import foreground from "../../assets/images/foreground-recyclable.png";
+import background from "../../assets/images/background-organic.png";
+import foreground from "../../assets/images/foreground-organic.png";
 
 export default function Guide() {
+	const title = "Organic";
 	return (
 		<>
-			<Banner backgroundURL={background} foregroundURL={foreground} />
-			<GuideInstructionsComp />
-			<GuideInstructionsComp />
+			<Banner
+				backgroundURL={background}
+				foregroundURL={foreground}
+				title={title}
+			/>
 			<GuideInstructionsComp />
 		</>
 	);
