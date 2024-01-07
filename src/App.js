936e33a6a79b3cbbd68deea65c1b0fe3791d60f3
Joshua George <joshua.vincent.george@gmail.com>
2024-01-07 02:16:50 -0800

    passed searchquery to categories page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Categories from "./pages/CategoriesPage/categories";
 import Guide from "./pages/GuidePage/GuidePage";
 import Navbar from "./components/Navbar/Navbar";
 import SplineView from "./components/splineView/SplineView";
+import { useState } from "react";
 function App() {
+	const [searchQuery, setSearchQuery] = useState("");
+
 	return (
 		<>
 			<BrowserRouter>
-				<Navbar />
+				<Navbar setSearchQuery={setSearchQuery} />
 				<div className="content">
 					<Routes>
 						{/* Home Route */}
 						<Route path="/" element={<Home />}></Route>
 						{/* Categories Route */}
-						<Route path="/categories" element={<Categories />}></Route>
+						<Route
+							path="/categories"
+							element={<Categories searchQuery={searchQuery} />}></Route>
 						<Route path="/categories/:id/guide" element={<Guide />}></Route>
 					</Routes>
 				</div>
