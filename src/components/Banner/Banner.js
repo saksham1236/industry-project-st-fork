05da89db0dc0df5acaa839a55a4fae4716fee5cf
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 19:17:49 -0800

    made text dynamic with title prop

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import "./banner.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-function Banner({ backgroundURL, foregroundURL }) {
+function Banner({ backgroundURL, foregroundURL, title }) {
 	const ref = useRef(null);
 	const { scrollYProgress } = useScroll({
 		target: ref,
@@ -14,7 +14,7 @@ function Banner({ backgroundURL, foregroundURL }) {
 	return (
 		<div ref={ref} className="banner">
 			<motion.div className="banner__text" style={{ y: textY }}>
-				Hello World!
+				{title}
 			</motion.div>
 			<div
 				className="banner__background"
