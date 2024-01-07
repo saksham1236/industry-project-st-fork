0ef8a4e3ead57ee937a12aaab1a490ed4c533c32
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 18:11:42 -0800

    imported banner

--- src/pages/GuidePage/GuidePage.js
+++ src/pages/GuidePage/GuidePage.js
@@ -1,11 +1,12 @@
 import "./guidePage.scss";
 import GuideInstructionsComp from "../../components/GuideInstructionsComp/guideInstructionsComp";
+import Banner from "../../components/Banner/Banner";
 
 export default function Guide() {
 	return (
 		<>
-			guide page
 			<GuideInstructionsComp />
+			<Banner />
 		</>
 	);
 }
