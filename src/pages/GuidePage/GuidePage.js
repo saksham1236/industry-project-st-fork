de11a8b629e31ca4c764cab61e2bcbe70480f509
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 18:57:05 -0800

    imported banner and added extra components for testing

--- src/pages/GuidePage/GuidePage.js
+++ src/pages/GuidePage/GuidePage.js
@@ -1,12 +1,16 @@
 import "./guidePage.scss";
 import GuideInstructionsComp from "../../components/GuideInstructionsComp/guideInstructionsComp";
 import Banner from "../../components/Banner/Banner";
+import background from "../../assets/images/background-Recyclable.png";
+import foreground from "../../assets/images/foreground-recyclable.png";
 
 export default function Guide() {
 	return (
 		<>
+			<Banner backgroundURL={background} foregroundURL={foreground} />
 			<GuideInstructionsComp />
-			<Banner />
+			<GuideInstructionsComp />
+			<GuideInstructionsComp />
 		</>
 	);
 }
