14636cf203007c7c9b426c21f48e6ab2c62a9522
Joshua George <joshua.vincent.george@gmail.com>
2024-01-06 21:45:37 -0800

    imported images and data from json and passed to children

--- src/pages/GuidePage/GuidePage.js
+++ src/pages/GuidePage/GuidePage.js
@@ -1,19 +1,38 @@
 import "./guidePage.scss";
 import GuideInstructionsComp from "../../components/GuideInstructionsComp/guideInstructionsComp";
 import Banner from "../../components/Banner/Banner";
-import background from "../../assets/images/background-organic.png";
-import foreground from "../../assets/images/foreground-organic.png";
+import backgroundHaz from "../../assets/images/background-hazardous.png";
+import foregroundHaz from "../../assets/images/foreground-hazardous.png";
+import backgroundOrg from "../../assets/images/background-organic.png";
+import foregroundOrg from "../../assets/images/foreground-organic.png";
+import { useParams } from "react-router";
+import Data from "../../data/guides.json";
 
 export default function Guide() {
 	const title = "Organic";
+
+	const { id } = useParams();
+
+	const bannerImages = [
+		{
+			id: "1",
+			bg: backgroundHaz,
+			fg: foregroundHaz,
+		},
+		{ id: "2", bg: backgroundOrg, fg: foregroundOrg },
+	];
+
+	const filterImages = bannerImages.filter((obj) => obj.id === id);
+	const filterData = Data.filter((data) => data.id === id);
+
 	return (
 		<>
 			<Banner
-				backgroundURL={background}
-				foregroundURL={foreground}
-				title={title}
+				backgroundURL={filterImages[0].bg}
+				foregroundURL={filterImages[0].fg}
+				title={filterData[0].title}
 			/>
-			<GuideInstructionsComp />
+			<GuideInstructionsComp id={id} data={filterData[0]} />
 		</>
 	);
 }
