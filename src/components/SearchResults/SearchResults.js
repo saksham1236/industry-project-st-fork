ef1d0f464269f3fc1b545a24c3e90c150fa3b7bb
Joshua George <joshua.vincent.george@gmail.com>
2024-01-07 02:27:24 -0800

    check if categories page added

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,21 @@
 import "./searchResults.scss";
 
-function SearchResults({ data }) {
-	return (
-		<div className="search-results">
-			{data.map((result, index) => {
-				return (
-					<div key={index} className="search-results__item">
-						{result.card_header}
-					</div>
-				);
-			})}
-		</div>
-	);
+function SearchResults({ data, isCategories }) {
+	if (isCategories) {
+		return <></>;
+	} else {
+		return (
+			<div className="search-results">
+				{data.map((result, index) => {
+					return (
+						<div key={index} className="search-results__item">
+							{result.card_header}
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default SearchResults;
