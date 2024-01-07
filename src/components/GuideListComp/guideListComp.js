b4b8db5f97f1fc4fdfbf2eb1b675f57ec1430800
Kate Bark <barkkate@gmail.com>
2024-01-07 15:02:26 -0500

    style list items

--- src/components/GuideListComp/guideListComp.js
+++ src/components/GuideListComp/guideListComp.js
@@ -1,14 +1,27 @@
-import "./guideListComp.scss"
+import "./guideListComp.scss";
+import CheckmarkIcon from "../../assets/icons/checkmark.svg";
+import XIcon from "../../assets/icons/x-icon.svg";
 
-function GuideListComponent({ steps }) {
-
+function GuideListComponent({ steps, addCheck, backgroundColor }) {
   return (
     <div className="guide-list">
       <div className="guide-list__list-container">
         {steps.map((step, index) => (
-          <p key={index} className="guide-list__step">
-            {step}
-          </p>
+          <div
+            className="guide-list__chip"
+            style={{
+              backgroundColor: `${backgroundColor}`
+            }}>
+            <img
+              src={addCheck ? CheckmarkIcon : XIcon}
+              className="guide-list__icon"
+              alt={addCheck ? `checkmark` : `what not to do`} />
+            <p key={index}
+              className="guide-list__step"
+            >
+              {step}
+            </p>
+          </div>
         ))}
       </div>
     </div>
