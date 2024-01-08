30f109c7960f4b5f6cc9a150abf2f86eb0c67e67
Joshua George <joshua.vincent.george@gmail.com>
2024-01-08 00:57:49 -0800

    passed data from cards.json

--- src/components/CategoryCard/categoryCard.js
+++ src/components/CategoryCard/categoryCard.js
@@ -1,25 +1,47 @@
 import "./categoryCard.scss";
 
 export default function CategoryCard(props) {
-  return (
-    <>
-      <div className="category--card" style={{backgroundColor: `${props.cardColor}`}}>
-        <h2 className="category__header" style={{color: `${props.textColor}`}}>{props.cardHeader}</h2>
-        <p className="category__description" style={{color: `${props.textColor}`}}>
-          {props.description}
-        </p>
-        <div className="image--container">
-          <div className="category__image">
-            <p className="image__text">{props.imageName}</p>
-          </div>
-          <div className="category__image">
-            <p className="image__text">Batteries</p>
-          </div>
-          <div className="category__image">
-            <p className="image__text">Batteries</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	console.log(props);
+
+	return (
+		<>
+			<div
+				className="category--card"
+				style={{ backgroundColor: `${props.cardColor}` }}>
+				<h2
+					className="category__header"
+					style={{ color: `${props.textColor}` }}>
+					{props.cardHeader}
+				</h2>
+				<p
+					className="category__description"
+					style={{ color: `${props.textColor}` }}>
+					{props.description}
+				</p>
+				<div className="image--container">
+					<div
+						className="category__image"
+						style={{
+							backgroundImage: `url(${props.imageOne})`,
+						}}>
+						<p className="image__text">{props.imageNameOne}</p>
+					</div>
+					<div
+						className="category__image"
+						style={{
+							backgroundImage: `url(${props.imageTwo})`,
+						}}>
+						<p className="image__text">{props.imageNameTwo}</p>
+					</div>
+					<div
+						className="category__image"
+						style={{
+							backgroundImage: `url(${props.imageThree})`,
+						}}>
+						<p className="image__text">{props.imageNameThree}</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/pages/CategoriesPage/categories.js
+++ src/pages/CategoriesPage/categories.js
@@ -1,98 +1,24 @@
 import "./categories.scss";
-import testImage from "../../assets/images/batteries.png";
 import CategoryCard from "../../components/CategoryCard/categoryCard";
-import cardData2 from "../../data/cards.json";
+import cardData from "../../data/cards.json";
 
 export default function Categories({ searchQuery }) {
-	const cardData = [
-		{
-			card_header: "Liquid",
-			card_color: "#29a6ff75",
-			text_color: "#00487b",
-			description:
-				"Liquid waste refers to all grease, oil, sludges, wash water, waste detergents and dirty water that have been thrown away. They are hazardous and poisonous to our environment and are found in industries as well as households. Wastewater, as it is often called, is any waste that exists in liquid form.",
-			imageData: [
-				{
-					id: "1",
-					image_one: { testImage },
-					image_name: "Batteries",
-				},
-				{
-					id: "2",
-					image_two: { testImage },
-					image_name: "cell phone",
-				},
-				{
-					id: "3",
-					image_three: { testImage },
-					image_name: "waste",
-				},
-			],
-		},
-		{
-			image_one: { testImage },
-			image_two: { testImage },
-			image_three: { testImage },
-			card_header: "Hazardous",
-			image_name: "Batteries",
-			card_color: "#fff8e8",
-			text_color: "#875b0f",
-			description:
-				"Hazardous waste disposal is essential for preventing environmental and health risks. Proper containment, treatment, and adherence to regulations ensure safe and responsible management of these materials.",
-		},
-		{
-			image_one: { testImage },
-			image_two: { testImage },
-			image_three: { testImage },
-			card_header: "Organic",
-			image_name: "Batteries",
-			card_color: "#60bd6066",
-			text_color: "#003400",
-			description:
-				"Organic waste refers to rotten meat, garden and food waste. This type of rubbish is commonly found in homes. With time, they decompose and turn into manure by the action of microorganisms on them. But be careful; you should not dispose of them anywhere you like.",
-		},
-		{
-			image_one: { testImage },
-			image_two: { testImage },
-			image_three: { testImage },
-			card_header: "Recyclable",
-			image_name: "Batteries",
-			card_color: "#2265FF",
-			text_color: "#002A8C",
-			description:
-				"All discarded items like metals, furniture, organic waste that can be recycled fall under this category. Not all items are recyclable, so you have to be careful when putting things into the recycle bin. If you are not sure whether an item is recyclable or not, then check the item’s packaging.",
-		},
-		{
-			image_one: { testImage },
-			image_two: { testImage },
-			image_three: { testImage },
-			card_header: "Solid",
-			image_name: "Batteries",
-			card_color: "#cbcaca",
-			text_color: "#686868",
-			description:
-				"Solid waste is any garbage, sludge, and refuse found in industrial and commercial locations. The five major types of solid rubbish are;",
-		},
-	];
-
 	const filteredData = cardData.filter((data) =>
 		data.card_header.toLowerCase().includes(searchQuery.toLowerCase())
 	);
 
-	const image = cardData2[0].imageData.image_one;
-	console.log(image);
-
 	return (
 		<>
-			<img src={image} />
 			{filteredData.map((data) => {
 				return (
 					<CategoryCard
-						imageOne={data.image_one}
-						imageTwo={data.image_two}
-						imageThree={data.image_three}
+						imageOne={data.imageData.image_one}
+						imageTwo={data.imageData.image_two}
+						imageThree={data.imageData.image_three}
 						cardHeader={data.card_header}
-						imageName={data.image_name}
+						imageNameOne={data.imageData.image_one_name}
+						imageNameTwo={data.imageData.image_two_name}
+						imageNameThree={data.imageData.image_three_name}
 						description={data.description}
 						cardColor={data.card_color}
 						textColor={data.text_color}
