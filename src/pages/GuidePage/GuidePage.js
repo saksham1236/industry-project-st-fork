81179c34c2ef60644581563d3ab2987a9fb989d7
Joshua George <joshua.vincent.george@gmail.com>
2024-01-08 01:10:13 -0800

    removed bannerImages and filter

--- src/pages/GuidePage/GuidePage.js
+++ src/pages/GuidePage/GuidePage.js
@@ -6,29 +6,18 @@ import { useParams } from "react-router";
 import Data from "../../data/guides.json";
 
 export default function Guide() {
-	// const { id } = useParams();
-
-	// const bannerImages = [
-	// 	{
-	// 		id: "1",
-	// 		bg: backgroundHaz,
-	// 		fg: foregroundHaz,
-	// 	},
-	// 	{ id: "2", bg: backgroundOrg, fg: foregroundOrg },
-	// ];
-
-	// const filterImages = bannerImages.filter((obj) => obj.id === id);
-	// const filterData = Data.filter((data) => data.id === id);
+	const { id } = useParams();
+	const filterData = Data.filter((data) => data.id === id);
 
 	return (
 		<>
-			{/* <Banner
-				backgroundURL={filterImages[0].bg}
-				foregroundURL={filterImages[0].fg}
+			<Banner
+				backgroundURL={filterData[0].bg}
+				foregroundURL={filterData[0].fg}
 				title={filterData[0].title}
 			/>
 			<GuideInstructionsComp data={filterData[0]} />
-			<GuideReasons data={filterData[0]} /> */}
+			<GuideReasons data={filterData[0]} />
 		</>
 	);
 }
