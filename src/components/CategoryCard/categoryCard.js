59c8e8941893d7357f37cb94d1674a2410da02bb
Matthew Pellettieri <mrpellettieri@gmail.comgit config --global user.email mrpellettieri@gmail.com>
2024-01-09 01:29:52 -0800

    life is pain

--- src/components/CategoryCard/categoryCard.js
+++ src/components/CategoryCard/categoryCard.js
@@ -2,9 +2,35 @@ import "./categoryCard.scss";
 import { Link } from "react-router-dom";
 
 export default function CategoryCard(props) {
+
+	const gradientOne = {
+		backgroundImage: `linear-gradient(
+			180deg,
+			rgba(0, 17, 42, 0) 58.51%,
+			rgba(0, 22, 47, 0.75) 100%
+		),
+		 url("${props.imageOne}")`
+	}
+	const gradientTwo = {
+		backgroundImage: `linear-gradient(
+			180deg,
+			rgba(0, 17, 42, 0) 58.51%,
+			rgba(0, 22, 47, 0.75) 100%
+		),
+		 url("${props.imageTwo}")`
+	}
+	const gradientThree = {
+		backgroundImage: `linear-gradient(
+			180deg,
+			rgba(0, 17, 42, 0) 58.51%,
+			rgba(0, 22, 47, 0.75) 100%
+		),
+		 url("${props.imageThree}")`
+	}
+
 	return (
 		<>
-			<Link to={props.url}>
+			<Link className="category__image--link" to={props.url}>
 				<div
 					className="category--card"
 					style={{ backgroundColor: `${props.cardColor}` }}>
@@ -21,23 +47,18 @@ export default function CategoryCard(props) {
 					<div className="image--container">
 						<div
 							className="category__image"
-							style={{
-								backgroundImage: `url(${props.imageOne})`,
-							}}>
+							style=
+								{gradientOne}>
 							<p className="image__text">{props.imageNameOne}</p>
 						</div>
 						<div
 							className="category__image"
-							style={{
-								backgroundImage: `url(${props.imageTwo})`,
-							}}>
+							style={gradientTwo}>
 							<p className="image__text">{props.imageNameTwo}</p>
 						</div>
 						<div
 							className="category__image"
-							style={{
-								backgroundImage: `url(${props.imageThree})`,
-							}}>
+							style={gradientThree}>
 							<p className="image__text">{props.imageNameThree}</p>
 						</div>
 					</div>
