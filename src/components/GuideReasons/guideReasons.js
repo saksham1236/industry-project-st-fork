68303b0a988178e109f249899620241257e102cc
Kate Bark <barkkate@gmail.com>
2024-01-09 14:57:30 -0500

    added icon bullet points to environ impact list

--- src/components/GuideReasons/guideReasons.js
+++ src/components/GuideReasons/guideReasons.js
@@ -7,13 +7,19 @@ function GuideReasons({ data }) {
 			style={{
 				backgroundColor: `${data.card_color}`,
 				color: `${data.text_color}`,
-			}}>
+			}}
+		>
 			<h1 className="guide-reasons__title">Environmental Impact</h1>
 			<div className="guide-reasons__container">
 				{data.reasons.map((reason, index) => (
-					<p key={index} className="guide-reasons__list-item">
-						{reason}
-					</p>
+					<div key={index} className="guide-reasons__list-item">
+						<img
+							src={reason.icon}
+							alt={`reason icon`}
+							className="guide-reasons__icon"
+						/>
+						<span>{reason.text}</span>
+					</div>
 				))}
 			</div>
 		</div>
